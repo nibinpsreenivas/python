0d026662461eb24d483c94ce687fa320ae1d878c
NIBIN <96453329+nibinpsreenivas@users.noreply.github.com>
2023-03-12 10:49:44 +0530

    Add files via upload

---
+++ cmp_no.py
@@ -0,0 +1,9 @@
+a=int(input('enter the first number'))
+b=int(input('enter the second number'))
+
+if a==b:
+    print('numbers are same')
+elif a>b:
+    print('first number is largest')
+else :
+    print('second number is largest')
